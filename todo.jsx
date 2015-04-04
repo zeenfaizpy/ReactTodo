fae7c82e36c7cc8998db0a7a852742627ae5e363
Mohammad Faizal <mfaizal@deam.ae>
2015-04-04 23:52:57 +0400

    adding state in parent

--- todo.jsx
+++ todo.jsx
@@ -1,16 +1,23 @@
 var React = require('react')
 var Inputbox = require('./inputbox')
 var Itemcontainer = require('./itemcontainer')
+var payload = require('./payload')
 
-var Hello = React.createClass({
+var Todo = React.createClass({
+    getInitialState: function() {
+        return payload
+    },
+    onUpdate: function(obj){
+        this.setState({items: this.state.items.concat([obj])});
+    },
     render: function() {
         return (
             <div>
-                <Inputbox />
-                <Itemcontainer />
+                <Inputbox onUpdate={this.onUpdate} />
+                <Itemcontainer items={this.state.items} />
             </div>
             )
     }
 })
 
-module.exports = Hello
+module.exports = Todo
