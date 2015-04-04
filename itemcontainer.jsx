f4ac33d6beda11fdea5f049855c5077c13568b2b
Mohammad Faizal <mfaizal@deam.ae>
2015-04-04 23:52:19 +0400

    fixing data flow from child to parent

--- itemcontainer.jsx
+++ itemcontainer.jsx
@@ -1,12 +1,12 @@
 var React = require('react')
 var Item = require('./item')
-var payload = require('./payload')
 
 var Itemcontainer = React.createClass({
+    
     render: function() {
         return (
             <ul>
-                {payload.map(function(todo_item){
+                {this.props.items.map(function(todo_item){
                     return <Item name={todo_item.name} />
                 })}
             </ul>
